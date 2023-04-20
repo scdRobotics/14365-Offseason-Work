db952c31ea67591e4097a5deaed8a712058a027c
SCD Robotics <43300604+scdRobotics@users.noreply.github.com>
2023-04-20 16:27:34 -0400

    Update DriveConstants for tuning process
    Done remotely to assist in-person meeting

--- drive/DriveConstants.java
+++ drive/DriveConstants.java
@@ -32,8 +32,8 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(29, 0, 20,
-            26);
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
+            10);
 
     /*
      * These are physical constants that can be determined from your robot (including the track
