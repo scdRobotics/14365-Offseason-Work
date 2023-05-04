ff0ce3ef481f3c28e234202373f9662ae8e721ab
SCD Robotics <43300604+scdRobotics@users.noreply.github.com>
2023-05-04 16:09:32 -0400

    The Robot still moves, sort of
    @EatYourChair The robot (Churro) can consistently move forward and backward. However, the wheels turn but he stays in place when I try to make him strafe. The motors turn in the correct directions, but I need to fix something in order to make him move. :)

--- _SimpleSquare.java
+++ _SimpleSquare.java
@@ -21,37 +21,14 @@ public class _SimpleSquare extends AUTO_PRIME {
         robot.drive.setPoseEstimate(penis);
 
         TrajectorySequence balls = robot.drive.trajectorySequenceBuilder(penis)
-//                .lineTo(new Vector2d(0, 20))
-                .splineToConstantHeading(new Vector2d(20, 0),Math.toRadians(0))
-                //.splineToLinearHeading(new Pose2d(0, 20),Math.toRadians((0)))
-                //.splineToLinearHeading(new Pose2d(0,0),Math.toRadians(0))
-                .build();
-
-        /*TrajectorySequence boobies = robot.drive.trajectorySequenceBuilder(balls.end())
-                .lineToSplineHeading(new Pose2d(20, 20, Math.toRadians(180)))
-                .build();
-
-        TrajectorySequence pp = robot.drive.trajectorySequenceBuilder(boobies.end())
                 .lineTo(new Vector2d(0, 20))
+                .lineTo(new Vector2d(20, 20))
                 .build();
-
-        TrajectorySequence cock = robot.drive.trajectorySequenceBuilder(pp.end())
-                .lineTo(new Vector2d(0,0))
-                .build();
-
-         */
 
 
         waitForStart();
 
         robot.drive.followTrajectorySequence(balls);
-        /*
-        robot.drive.followTrajectorySequence(boobies);
-        robot.drive.followTrajectorySequence(pp);
-        robot.drive.followTrajectorySequence(cock);
-
-         */
-
 
 
     }
